c6d66594f23e920d7a2605d22138be70af001739
thedavidmeister <thedavidmeister@gmail.com>
2024-02-28 07:07:57 +0400

    rename deploy

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -4,11 +4,11 @@ pragma solidity =0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {CloneFactory} from "src/concrete/CloneFactory.sol";
 
-/// @title DeployCloneFactory
+/// @title Deploy
 /// @notice A script that deploys a CloneFactory. This is intended to be run on
 /// every commit by CI to a testnet such as mumbai, then cross chain deployed to
 /// whatever mainnet is required, by users.
-contract DeployCloneFactory is Script {
+contract Deploy is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYMENT_KEY");
 
