cd80a33d292acd927cde75b1fe28f11f24395e5d
thedavidmeister <thedavidmeister@gmail.com>
2024-02-28 04:49:18 +0400

    update rainix

--- flake.nix
+++ flake.nix
@@ -2,25 +2,31 @@
   description = "Flake for development workflows.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    rain.url = "github:rainprotocol/rain.cli";
     flake-utils.url = "github:numtide/flake-utils";
+    rainix.url = "github:rainprotocol/rainix";
   };
 
-  outputs = {self, nixpkgs, rain, flake-utils }:
+  outputs = {self, rainix, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        rain-cli = "${rain.defaultPackage.${system}}/bin/rain";
-
+        pkgs = rainix.pkgs.${system};
       in rec {
         packages = rec {
-          build-meta = pkgs.writeShellScriptBin "build-meta" ''
+          factory-prelude = pkgs.writeShellScriptBin "factory-prelude" ''
             forge build --force && \
-            ${rain-cli} meta build -o meta/CloneFactory.rain.meta -i <(${rain-cli} meta solc artifact -c abi -i out/CloneFactory.sol/CloneFactory.json) -m solidity-abi-v2 -t json -e deflate -l en
+            rain meta build \
+              -o meta/CloneFactory.rain.meta \
+              -i <(rain meta solc artifact -c abi -i out/CloneFactory.sol/CloneFactory.json) \
+              -m solidity-abi-v2 \
+              -t json \
+              -e deflate \
+              -l en
           '';
+        };
 
-          default = build-meta;
+        devShells.default = pkgs.mkShell {
+          packages = [ packages.factory-prelude ];
+          inputsFrom = [ rainix.devShells.${system}.default ];
         };
       }
     );
