95b27d2856d415e5cf1829f1091a73a9310126d6
thedavidmeister <thedavidmeister@gmail.com>
2026-02-10 19:08:14 +0400

    deployed code hash

--- src/lib/LibCloneFactoryDeploy.sol
+++ src/lib/LibCloneFactoryDeploy.sol
@@ -5,5 +5,5 @@ pragma solidity ^0.8.25;
 library LibCloneFactoryDeploy {
     address constant CLONE_FACTORY_DEPLOYED_ADDRESS = address(0xFF33804957D415F0BDCC5A5314D88125F1d3beee);
 
-    bytes32 constant CLONE_FACTORY_DEPLOYED_CODEHASH = bytes32(0);
+    bytes32 constant CLONE_FACTORY_DEPLOYED_CODEHASH = bytes32(0xe492c748e3f4247a6a99f0703c29bc14f662f4716c3faa6b8ef33f8ac5a691eb);
 }
